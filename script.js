1e97aa20adf577558d5e14e3414b71d9fa96653c
David Stinnett <davidstinnett@icloud.com>
2020-09-20 14:44:02 -0400

    Finalized Project

---
+++ script.js
@@ -0,0 +1,176 @@
+// Declare variables//
+
+let timeNow = moment().format('dddd, MMMM Do YYYY');
+let currentTime = moment().format('hh:mm')
+let hourNow = moment().format('h');
+let hourNow24= parseInt(moment().format('HH'));
+let hourNowInt = parseInt(hourNow);
+let Timer;
+
+const hour1 = $('#time1').text();
+const hour2 = $('#time2').text();
+const hour3 = $('#time3').text();
+const hour4 = $('#time4').text();
+const hour5 = $('#time5').text();
+const hour6 = $('#time6').text();
+const hour7 = $('#time7').text();
+const hour8 = $('#time8').text();
+const hour9 = $('#time9').text();
+
+const hour1Int = parseInt(hour1);
+const hour2Int = parseInt(hour2);
+const hour3Int = parseInt(hour3);
+const hour4Int = parseInt(hour4);
+const hour5Int = parseInt(hour5);
+const hour6Int = parseInt(hour6);
+const hour7Int = parseInt(hour7);
+const hour8Int = parseInt(hour8);
+const hour9Int = parseInt(hour9);
+
+//Store user input in local Storage//
+
+$('#btn1').click(function() {
+  if($('#text1').val()) {
+    localStorage.removeItem('text1');
+  }
+
+  const hour1String = JSON.stringify(hour1);
+  
+  localStorage.setItem(hour1, $('#text1').val());
+
+});
+
+
+$('#btn2').click(function() {
+  if($('#text2').val()) {
+    localStorage.removeItem('text2');
+  }
+
+  const hour2String = JSON.stringify(hour2);
+
+  localStorage.setItem(hour2, $('#text2').val());
+});
+
+$('#btn3').click(function() {
+  if($('#text3').val()) {
+    localStorage.removeItem('text3');
+  }
+
+  const hour3String = JSON.stringify(hour3);
+
+  localStorage.setItem(hour3, $('#text3').val());
+});
+
+$('#btn4').click(function() {
+  if($('#text4').val()) {
+    localStorage.removeItem('text4');
+  }
+
+  const hour4String = JSON.stringify(hour4);
+
+  localStorage.setItem(hour4, $('#text4').val());
+});
+
+$('#btn5').click(function() {
+  if($('#text5').val()) {
+    localStorage.removeItem('text5');
+  }
+
+  const hour5String = JSON.stringify(hour5);
+
+  localStorage.setItem(hour5, $('#text5').val());
+});
+
+$('#btn6').click(function() {
+  if($('#text6').val()) {
+    localStorage.removeItem('text6');
+  }
+
+  const hour6String = JSON.stringify(hour6);
+
+  localStorage.setItem(hour6, $('#text6').val());
+});
+
+$('#btn7').click(function() {
+  if($('#text7').val()) {
+    localStorage.removeItem('text7');
+  }
+
+  const hour7String = JSON.stringify(hour7);
+
+  localStorage.setItem(hour7, $('#text7').val());
+})
+
+$('#btn8').click(function() {
+  if($('#text8').val()) {
+    localStorage.removeItem('text8');
+  }
+
+  const hour8String = JSON.stringify(hour8);
+
+  localStorage.setItem(hour8, $('#text8').val());
+})
+
+$('#btn9').click(function() { 
+  if($('#text9').val()) {
+    localStorage.removeItem('text9');
+  }
+
+  const hour9String = JSON.stringify(hour9);
+
+  localStorage.setItem(hour9, $('#text9').val());
+})
+
+//displays local storage items on planner//
+document.getElementById("text1").innerHTML = localStorage.getItem(hour1)
+document.getElementById("text2").innerHTML = localStorage.getItem(hour2)
+document.getElementById("text3").innerHTML = localStorage.getItem(hour3)
+document.getElementById("text4").innerHTML = localStorage.getItem(hour4)
+document.getElementById("text5").innerHTML = localStorage.getItem(hour5)
+document.getElementById("text6").innerHTML = localStorage.getItem(hour6)
+document.getElementById("text7").innerHTML = localStorage.getItem(hour7)
+document.getElementById("text8").innerHTML = localStorage.getItem(hour8)
+document.getElementById("text9").innerHTML = localStorage.getItem(hour9)
+
+// Display the time using moment.js//
+
+$('#currentDay').append(timeNow);
+$('#currentTime').append(currentTime)
+
+
+//Color coding to reflect whether the time slot is in the past, the present or the future, defaults to pink if outside of active work hours//
+
+colorCoding();
+
+function colorCoding() {
+
+  TIMER = setInterval(colorCoding, 1000);
+  // Test check: hourNow24 = 20;
+  if(hourNow24 >= 9 && hourNow24 <= 17) {
+
+    for (let i =1; i<=9 ; i++) { 
+     let hourInInt = parseInt($('#time'+i).text());
+
+      if (hourInInt < 9) {
+        hourInInt = hourInInt + 12;
+      }
+      
+      if (hourInInt == hourNow24) {
+        $('#text'+i).css('background-color', '#FB8F78');
+        continue;
+      }
+      
+      if (hourInInt < hourNow24) {
+        $('#text'+i).css('background-color', 'lightgray');
+      } 
+      else {
+        $('#text'+i).css('background-color', 'lightgreen');
+      }
+    }
+
+  }
+  else {
+    clearInterval(TIMER);
+    $('textarea').css('background-color', 'pink');
+  }
+}
