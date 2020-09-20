cd88d6c401673ed0e43694ddd05ab53fbaed7df6
David Stinnett <davidstinnett@icloud.com>
2020-09-20 18:04:21 -0400

    refactored code

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ const appointments = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM",
 
 //initialize application by pulling items from local storage//
 function init() {
-    $(document).ready(function(){
-        for (var j = 9; j < 18; j++){
-            $(".content").eq(j-9).val(localStorage.getItem(appointments[j-9]));
-        }
-    });
+  $(document).ready(function () {
+    for (var j = 9; j < 18; j++) {
+      $(".content").eq(j - 9).val(localStorage.getItem(appointments[j - 9]));
+    }
+  });
 }
 
 init();
 
 //save function to save items to local storage//
-$(".save").click(function(){
+$(".save").click(function () {
   var appointment = $(this).siblings(".content").val();
   var timeSlot = $(this).siblings(".timeValue").attr("id");
 
@@ -32,13 +32,13 @@ $('#currentTime').append(clock)
 //Color coding to reflect whether the time slot is in the past, the present or the future//
 
 for (var i = 9; i < 18; i++) {
-  if (currentTime > i){
-      $(".content").eq(i-9).css("background-color", "lightgrey");
+  if (currentTime > i) {
+    $(".content").eq(i - 9).css("background-color", "lightgrey");
   }
-  if (currentTime === i){
-      $(".content").eq(i-9).css("background-color", "pink");
+  if (currentTime === i) {
+    $(".content").eq(i - 9).css("background-color", "pink");
   }
-  if (currentTime < i){
-      $(".content").eq(i-9).css("background-color", "green");
+  if (currentTime < i) {
+    $(".content").eq(i - 9).css("background-color", "green");
   }
 }
